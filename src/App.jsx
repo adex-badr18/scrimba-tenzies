0e601ed86a7a16a90ef7ef660d96e0735d3aeb16
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-06 21:33:19 +0100

    Render ten die components with random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import Die from './components/Die';
 
 function App() {
+    function getTenRandomNumbers() {
+        const diceArr = [];
+        for (let i = 0; i < 10; i++) {
+            diceArr.push(Math.floor(Math.random() * 6) + 1);
+        }
+
+        return diceArr;
+    }
+
+    const diceElements = getTenRandomNumbers().map((number, index) => <Die key={index} value={number} />)
+
     return (
         <main className='container'>
             <div className="tenzies--container">
@@ -9,7 +21,10 @@ function App() {
                 <p>
                     Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
                 </p>
-                
+
+                <div className="dice-container">
+                    {diceElements}
+                </div>
             </div>
         </main>
     )
